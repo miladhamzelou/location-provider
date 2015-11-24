eba31c098a09e06e6935f1a244ab1577c68a6fea
milad <milad.h@mabna.mobi>
2015-11-24 11:52:36 +0330

    add

--- src/db.php
+++ src/db.php
@@ -9,23 +9,24 @@
 namespace Mabna\Location;
 
 
- use Illuminate\Database\Capsule\Manager as Capsule;
- use Illuminate\Events\Dispatcher;
- use Illuminate\Container\Container;
  class db
 {
     public static function prepareDatabase()
     {
+
+
+
+
         $capsule = new Capsule();
         $capsule->addConnection([
-        'driver'    => 'mysql',
-        'host'      => 'localhost',
-        'database'  => 'geo',
-        'username'  => 'root',
-        'password'  => 'iGlL]m1B',
-        'charset'   => 'utf8',
-        'collation' => 'utf8_general_ci',
-        'prefix'    => '']);
+            'driver'    => 'mysql',
+            'host'      => Config::get('database.connections.mysql.host'),
+            'database'  => Config::get('database.connections.mysql.database'),
+            'username'  => Config::get('database.connections.mysql.username'),
+            'password'  => Config::get('database.connections.mysql.password'),
+            'charset'   => Config::get('database.connections.mysql.charset'),
+            'collation' => Config::get('database.connections.mysql.collation'),
+            'prefix'    => Config::get('database.connections.mysql.prefix')]);
 
         $capsule->setEventDispatcher(new Dispatcher(new Container));
         $capsule->setAsGlobal();
