bf752b6763b6fb52aefbe222ce15475c5aba4494
milad <milad.h@mabna.mobi>
2015-11-24 09:41:14 +0330

    add

---
+++ src/repository/ModelRepository.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: milad
+ * Date: 11/1/15
+ * Time: 9:11 AM
+ */
+
+namespace geo\src\repository;
+
+use Prettus\Repository\Eloquent\BaseRepository;
+
+class ModelRepository extends BaseRepository
+{
+
+    public $model;
+
+    function __construct($model)
+    {
+        $model = new $model();
+        $this->model = $model;
+        $this->model();
+    }
+
+    function model()
+    {
+        return $this->model ;
+    }
+}
